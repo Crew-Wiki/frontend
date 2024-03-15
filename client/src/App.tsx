609ed08dea5feabf097bea9cbd90c149dee0959f
김진호 <rlawlsgh1227@gmail.com>
2024-03-15 22:08:18 +0900

    chore(toast-editor): 마크다운 편집기 초기셋팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Button from '@components/Button';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 function App() {
   return (
     <div className="App">
       <div className="text-3xl font-bold bg-black">안녕하세요</div>
+      <Editor
+        initialValue="hello react editor world!"
+        previewStyle="vertical"
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={false}
+      />
       <Button />
     </div>
   );
---
+++ client/src/components/markdownEditor/TuiEditor.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/toastui-editor.css';
+
+type HookCallback = (url: string, text?: string) => void;
+
+type Props = {
+  editorRef: React.RefObject<Editor> | null;
+  imageHandler: (blob: File | Blob, callback: HookCallback) => void;
+  content?: string;
+};
+
+const toolbar = [['heading', 'bold', 'italic', 'strike'], ['hr', 'quote', 'ul', 'ol'], ['image']];
+
+function TuiEditor({ content, editorRef, imageHandler }: Props) {
+  return (
+    <Editor
+      initialValue={content ?? ' '}
+      initialEditType="wysiwyg"
+      autofocus={false}
+      ref={editorRef}
+      toolbarItems={toolbar}
+      hideModeSwitch
+      height="500px"
+      hooks={{ addImageBlobHook: imageHandler }}
+    />
+  );
+}
+
+export default TuiEditor;
