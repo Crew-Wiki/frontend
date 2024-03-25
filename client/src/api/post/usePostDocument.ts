38414f7b4aadde03faff638474482f6ed2703c05
김진호 <rlawlsgh1227@gmail.com>
2024-03-25 21:43:53 +0900

    feat: post document api 작성

--- client/src/api/post/usePostDocument.ts
+++ client/src/api/post/usePostDocument.ts
@@ -1,3 +1,55 @@
-const usePostDocument = () => {};
+import axiosInstance from '@/utils/axios';
+import ENDPOINT from '@/constants/endpoint';
+import KEYS from '@/constants/keys';
+import { useMutation } from '@tanstack/react-query';
+
+const { POST_DOCUMENT } = ENDPOINT;
+const { QUERY } = KEYS;
+
+// 요청할 때 필요한 데이터
+interface PostDocumentContent {
+  title: string;
+  contents: string;
+  writer: string;
+}
+
+// Request body 타입
+interface PostDocumentRequest {
+  contents: string;
+  writer: string;
+}
+
+// Response body 타입
+interface PostDocumentResponse {
+  documentId: number;
+  title: string;
+  contents: string;
+  writer: string;
+  generateTime: string;
+}
+
+const usePostDocument = () => {
+  const writeDocument = async ({ title, contents, writer }: PostDocumentContent) => {
+    const body = { contents, writer };
+    const response = await axiosInstance.post<PostDocumentRequest, PostDocumentResponse>(
+      `${POST_DOCUMENT}/${title}`,
+      body,
+    );
+    return response;
+  };
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: [QUERY.POST_DOCUMENT],
+    mutationFn: writeDocument,
+    onSuccess: (response) => {
+      console.log(response);
+    },
+  });
+
+  return {
+    writeDocument: mutate,
+    isPending,
+  };
+};
 
 export default usePostDocument;
---
+++ client/src/constants/keys.ts
@@ -0,0 +1,9 @@
+const QUERY = {
+  POST_DOCUMENT: 'post-document',
+};
+
+const KEYS = {
+  QUERY,
+};
+
+export default KEYS;
