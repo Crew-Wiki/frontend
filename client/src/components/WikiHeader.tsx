fe1ce6ac8c0f41e3fc53dd188f66607a611312bb
skiende74 <skiende74@gmail.com>
2024-03-28 21:14:17 +0900

    feat(WikiHeader): 위키 헤더 검색창 반응형 적용.

--- client/src/components/WikiHeader.tsx
+++ client/src/components/WikiHeader.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import WikiInputField from './WikiInputField';
+import { ReactComponent as SearchCircleSmall } from '@assets/image/search-circle.svg';
+
 
 const WikiHeader = () => {
   return (
-    <header className="flex w-ful h-20 bg-primary-primary justify-center">
+    <header className="flex w-full h-20 bg-primary-primary justify-center">
       <div className="flex justify-between items-center px-4 header-container max-w-[1440px] w-full">
         <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
-        <WikiInputField />
+        <WikiInputField className="w-20 md:w-[20.25rem] hidden md:flex" />
+        <SearchCircleSmall className="cursor-pointer md:hidden" />
       </div>
     </header>
   );
--- client/src/components/WikiInputField.tsx
+++ client/src/components/WikiInputField.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ReactComponent as SearchCircle } from '@assets/image/search-circle-secondary.svg';
+import { ReactComponent as SearchCircleSmall } from '@assets/image/search-circle.svg';
+import { twMerge } from 'tailwind-merge';
 
-const WikiInputField = () => {
+interface WikiInputProps {
+  className?:string
+}
+
+const WikiInputField = ({className}:WikiInputProps) => {
   return (
-    <div className="flex w-80 h-11 px-4 py-2.5 rounded-xl bg-white border-grayscale-200 border-solid border gap-2">
+    <div className={twMerge("flex w-80 h-11 px-4 py-2.5 rounded-xl bg-white border-grayscale-200 border-solid border gap-2", className)}>
       <input
-        className="w-full outline-none font-pretendard text-base font-normal text-grayscale-800 placeholder:text-grayscale-lightText"
+        className="w-full outline-none font-pretendard text-base font-normal text-grayscale-800 placeholder:text-grayscale-lightText "
         placeholder="검색할 문서의 제목을 입력하세요."
       />
       <SearchCircle className="cursor-pointer" />
