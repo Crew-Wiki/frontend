58f546fd30189c101f00d4f53a53e386eeaabd9b
김진호 <rlawlsgh1227@gmail.com>
2024-03-29 13:11:56 +0900

    feat: 로고 누르면 대문으로, 에디터에 링크 기능 추가

--- client/src/components/WikiHeader.tsx
+++ client/src/components/WikiHeader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import WikiInputField from './WikiInputField';
 
 const WikiHeader = () => {
   return (
     <header className="flex w-ful h-20 bg-primary-primary justify-center">
       <div className="flex justify-between items-center px-4 header-container max-w-[1440px] w-full">
-        <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
+        <Link to="/">
+          <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
+        </Link>
         <WikiInputField />
       </div>
     </header>
