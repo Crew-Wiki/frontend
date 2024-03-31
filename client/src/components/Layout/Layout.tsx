01a7ecbd19ee8dea9377e2143109242c31009568
이태훈 <rhymint@gmail.com>
2024-03-31 17:14:04 +0900

    feat: scroll 감지하여 header 숨기기 및 보이기 기능 추가

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
 import RecentlyEditWrapper from '@components/RecentlyEditWrapper';
 import WikiHeader from '../WikiHeader';
 
+interface ScrollPosition {
+  prev: number;
+  current: number;
+}
+
 const Layout = () => {
+  const [scrollPosition, setScrollPosition] = useState<ScrollPosition>({
+    prev: window.scrollY,
+    current: window.scrollY,
+  });
+  const [showHeader, setShowHeader] = useState(true);
+
+  const handleScroll = () => {
+    setScrollPosition({ prev: scrollPosition.current, current: window.scrollY });
+    setShowHeader(scrollPosition.prev >= scrollPosition.current);
+    if (window.scrollY < 50) {
+      setShowHeader(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [window.scrollY]);
+
   return (
     <div className="App bg-grayscale-50">
-      <WikiHeader />
-      <div className="flex items-center justify-center h-fit">
+      <WikiHeader isShown={showHeader} />
+      <div className="flex items-center justify-center h-fit mt-20 max-[768px]:mt-12">
         <main className="flex gap-6 py-6 px-4 max-w-[1440px] w-full max-[768px]:py-2 max-[768px]:px-0">
           <div className="flex flex-col gap-6 w-full max-[768px]:gap-2">
             <Outlet />
--- client/src/components/WikiHeader.tsx
+++ client/src/components/WikiHeader.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { ReactComponent as SearchCircleSmall } from '@assets/image/search-circle.svg';
 import WikiInputField from './WikiInputField';
 
-const WikiHeader = () => {
+interface WikiHeaderProps {
+  isShown: boolean;
+}
+
+const WikiHeader = ({ isShown }: WikiHeaderProps) => {
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    if (isShown) {
+      setY(0);
+    } else {
+      setY(-80);
+    }
+  }, [isShown]);
+
   return (
-    <header className="flex w-full h-12 md:h-20 bg-primary-primary justify-center">
+    <motion.div
+      className="fixed top-0 flex w-full h-12 md:h-20 bg-primary-primary justify-center"
+      animate={{ y }}
+      transition={{ duration: 0.3 }}
+    >
       <div className="flex justify-between items-center px-4 header-container max-w-[1440px] w-full">
         <Link to="/">
           <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
@@ -13,7 +32,7 @@ const WikiHeader = () => {
         <WikiInputField className="w-20 md:w-[20.25rem] hidden md:flex" />
         <SearchCircleSmall className="cursor-pointer md:hidden" />
       </div>
-    </header>
+    </motion.div>
   );
 };
 
