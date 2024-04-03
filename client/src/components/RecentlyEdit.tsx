84eceff190a5f9322ec4672bc9691cf9535d3b2c
이태훈 <rhymint@gmail.com>
2024-04-03 17:04:26 +0900

    design: edit recentlyEdit font color

--- client/src/components/RecentlyEdit.tsx
+++ client/src/components/RecentlyEdit.tsx
@@ -9,14 +9,14 @@ const RecentlyEdit = () => {
 
   return (
     <aside className="max-[1024px]:hidden flex flex-col w-60 h-fit bg-white border-primary-100 border-solid border rounded-xl">
-      <h2 className="flex justify-center items-center w-full h-12 font-pretendard font-bold text-lg border-b border-primary-100">
+      <h2 className="flex justify-center items-center w-full h-12 font-pretendard font-bold text-lg border-b border-primary-100 text-grayscale-800">
         최근 편집
       </h2>
       {recentlyDocuments.documents.map((document) => {
         return (
           <Link
             key={`recently-${document.documentId}`}
-            className="px-2.5 py-2 font-pretendard font-normal text-xs border-b border-grayscale-100 last:border-0"
+            className="px-2.5 py-2 font-pretendard font-normal text-xs border-b border-grayscale-100 last:border-0 text-grayscale-800"
             to={`${URLS.WIKI}/${document.title}`}
           >
             {`[${timeConverter(document.generateTime, 'YYYY.MM.DD')}] ${document.title}`}
