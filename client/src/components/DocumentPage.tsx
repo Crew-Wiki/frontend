626e370171988cd28993d1a055ad0b2f8ea64f36
skiende74 <skiende74@gmail.com>
2024-04-03 16:38:58 +0900

    feat(DocumentPage): 모바일 화면에서 편집버튼이 본문이 아니라 본문 위에 뜨게 변경.

--- client/src/components/DocumentPage.tsx
+++ client/src/components/DocumentPage.tsx
@@ -4,6 +4,9 @@ import useGetDocumentByTitle from '@api/get/useGetDocumentByTitle';
 import DocumentHeader from './DocumentHeader';
 import DocumentContents from './DocumentContents';
 import DocumentFooter from './DocumentFooter';
+import { useQueryClient } from '@tanstack/react-query';
+import URLS from '@constants/urls';
+import Button from './Button';
 
 interface DocumentPageProps {
   daemoon?: string;
@@ -21,8 +24,36 @@ const DocumentPage = ({ daemoon }: DocumentPageProps) => {
     }
   }, [daemoon]);
 
+
+  const queryClient = useQueryClient();
+
+  const refreshData = () => {
+    queryClient.removeQueries();
+    navigate('');
+  };
+
+  const goPostPage = () => {
+    navigate(URLS.POST);
+  };
+
+  const goEditPage = () => {
+    navigate(URLS.EDIT, { state: docs });
+  };
+
+  const goLogsPage = () => {
+    navigate(URLS.LOGS, { state: docs });
+  };
+
   return (
     <>
+      <div className="md:hidden">
+        <fieldset className="flex gap-2 max-[768px]:w-full max-[768px]:justify-center">
+          <Button style="tertiary" size="xs" text="새로고침" onClick={refreshData} />
+          <Button style="tertiary" size="xs" text="편집하기" onClick={goEditPage} />
+          <Button style="tertiary" size="xs" text="편집로그" onClick={goLogsPage} />
+          <Button style="primary" size="xs" text="작성하기" onClick={goPostPage} />
+        </fieldset>
+      </div>
       <div className="flex flex-col gap-6 w-full h-fit min-h-[864px] bg-white border-primary-100 border-solid border rounded-xl p-8 max-[768px]:p-4 max-[768px]:gap-2">
         <DocumentHeader wiki={docs} />
         <DocumentContents contents={docs.contents} />
