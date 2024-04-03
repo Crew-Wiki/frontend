806712fb2144243afc2b8d00c99af80f8333674a
김진호 <rlawlsgh1227@gmail.com>
2024-04-03 18:09:06 +0900

    feat: 없는 문서일 때 작성하기 버튼 보이도록

--- client/src/components/DocumentFallback.tsx
+++ client/src/components/DocumentFallback.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { AxiosError } from 'axios';
 
 import { FallbackProps } from 'react-error-boundary';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import URLS from '@constants/urls';
 import DocumentTitle from './DocumentTitle';
+import Button from './Button';
 
 interface Error {
   errorMessage: string;
@@ -13,12 +15,20 @@ const DocumentFallback = ({ error }: FallbackProps) => {
   const axiosError = error as AxiosError;
   const errorDetail = axiosError.response?.data as Error;
 
+  const navigate = useNavigate();
   const { title } = useParams();
 
+  const goPostPage = () => {
+    navigate(URLS.POST);
+  };
+
   return (
-    <div className="flex flex-col gap-6 w-full h-fit bg-white border-primary-100 border-solid border rounded-xl p-8">
-      <header className="flex justify-between w-full">
+    <div className="flex flex-col gap-6 w-full h-fit min-h-[864px] bg-white border-primary-100 border-solid border rounded-xl p-8 max-[768px]:p-4 max-[768px]:gap-2">
+      <header className="max-[768px]:flex-col-reverse max-[768px]:gap-4 flex justify-between w-full">
         <DocumentTitle title={title ?? ''} />
+        <fieldset className="flex gap-2 max-[768px]:w-full max-[768px]:justify-center">
+          <Button style="primary" size="xs" text="작성하기" onClick={goPostPage} />
+        </fieldset>
       </header>
       {errorDetail.errorMessage}
     </div>
