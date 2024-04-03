20576604f2fc2ebd28fcdb668d6288114d97a977
이태훈 <rhymint@gmail.com>
2024-04-03 14:55:15 +0900

    design: header에 행성이 logo 추가

--- client/src/components/WikiHeader.tsx
+++ client/src/components/WikiHeader.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { ReactComponent as SearchCircleSmall } from '@assets/image/search-circle.svg';
+import LogoImage from '@assets/image/hangseong-white.png';
 import WikiInputField from './WikiInputField';
 
 interface ScrollPosition {
@@ -47,8 +48,9 @@ const WikiHeader = () => {
       transition={{ duration: 0.3 }}
     >
       <div className="flex justify-between items-center px-4 header-container max-w-[1440px] w-full">
-        <Link to="/">
-          <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
+        <Link to="/" className="flex gap-2 items-center">
+          <img src={LogoImage} alt="logo" className="h-10 md:h-16" />
+          <h1 className="font-bm text-2xl md:text-[40px] text-white font-normal">크루위키</h1>
         </Link>
         <WikiInputField className="w-20 md:w-[20.25rem] hidden md:flex" />
         <SearchCircleSmall className="cursor-pointer md:hidden" />
