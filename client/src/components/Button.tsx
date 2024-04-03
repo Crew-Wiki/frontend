6fd535c9e63baa98b725547c677915042e18a206
skiende74 <skiende74@gmail.com>
2024-04-03 14:06:31 +0900

    feat(Button): 버튼의 스타일을 state에 따라 바뀌게끔 구현. 버튼 state useState

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
+
+type ButtonState = 'default'|'disabled'|'pressed';
+type ButtonStyle = 'primary' | 'secondary' | 'tertiary' | 'text';
 
 interface ButtonProps {
   size: 'm' | 's' | 'xs';
-  style: 'primary' | 'secondary' | 'tertiary' | 'text';
+  style: ButtonStyle;
   text: string;
   type?: 'button' | 'reset' | 'submit';
   disabled?: boolean;
@@ -11,21 +14,47 @@ interface ButtonProps {
 }
 
 const Button = ({ size, type, style, text, disabled, onClick }: ButtonProps) => {
+
+  const [buttonState, setButtonState] = useState<ButtonState>('default');
+  if (disabled && buttonState !== 'disabled'){
+    setButtonState('disabled');
+  }
+  if (!disabled && buttonState === 'disabled') {
+    setButtonState('default');
+  }
+
   const BUTTONSIZE = {
     xs: 'h-9 rounded-[1.125rem] px-3',
     s: 'h-11 rounded-[1.375rem] px-4',
     m: 'h-14 rounded-[1.75rem] px-4',
   };
-  const BUTTONSTYLE = {
+  
+  const BUTTON_STYLE:Record<ButtonState, Record<ButtonStyle,string>> = {
+    default:{
     primary: 'bg-primary-primary text-white',
     secondary: 'bg-white text-primary-primary border-primary-primary border-solid border',
     tertiary: 'bg-white text-grayscale-lightText border-grayscale-border border-solid border',
-    text: 'bg-white text-primary-primary shadow-md',
+    text: 'bg-white text-primary-primary shadow-md'
+      },
+    disabled:{
+      primary: 'bg-grayscale-100 text-grayscale-400',
+    secondary: 'bg-grayscale-50 text-grayscale-400 border-grayscale-100 border-solid border',
+    tertiary: 'bg-grayscale-50 text-grayscale-400 border-grayscale-100 border-grayscale-border border-solid border',
+    text: 'bg-grayscale-100 text-grayscale-400 shadow-md'
+      },
+    pressed:{
+      primary: 'bg-grayscale-100 ',
+      secondary: 'bg-grayscale-100 ',
+      tertiary: 'bg-grayscale-100 ',
+      text: 'bg-grayscale-100 '
+    }
   };
+
+
   return (
     <button
       type={type}
-      className={twMerge('font-bm text-sm', BUTTONSIZE[size], BUTTONSTYLE[style])}
+      className={twMerge('font-bm text-sm', BUTTONSIZE[size], BUTTON_STYLE[buttonState][style])}
       disabled={disabled}
       onClick={onClick}
     >
