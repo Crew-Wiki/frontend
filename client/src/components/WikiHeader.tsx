af55a1158f2b2d3d68286b2d07bdab5b5700860a
skiende74 <skiende74@gmail.com>
2024-04-03 17:05:32 +0900

    feat(WikiHeader): 모바일환경에서 검색로고 클릭시 검색바 등장

--- client/src/components/WikiHeader.tsx
+++ client/src/components/WikiHeader.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as SearchCircleSmall } from '@assets/image/search-circle.svg';
 import RandomButton from '@assets/image/RandomButton';
 import WikiInputField from './WikiInputField';
+import { twMerge } from 'tailwind-merge';
 
 interface ScrollPosition {
   prev: number;
@@ -33,33 +34,43 @@ const WikiHeader = () => {
     };
   }, [window.scrollY]);
 
+
+  const ESTIMATED_HEADER_HEIGHT = 80;
   useEffect(() => {
     if (showHeader) {
       setY(0);
     } else {
-      setY(-80);
+      setY(-ESTIMATED_HEADER_HEIGHT);
     }
   }, [showHeader]);
 
+  
+  const [isVisibleSmallSearchBar,setVisibleSmallSearchBar] = useState(false);
+
   return (
     <motion.div
-      className="fixed top-0 flex w-full h-12 md:h-20 bg-primary-primary justify-center"
+      className="fixed top-0 w-full gap-y-4 py-4 bg-primary-primary "
       animate={{ y }}
       transition={{ duration: 0.3 }}
     >
-      <div className="flex justify-between items-center px-4 header-container max-w-[1440px] w-full">
-        <Link to="/">
-          <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
-        </Link>
+      <div className="flex flex-col justify-center items-center gap-y-4 py-4">
+        <div className="flex flex-row justify-between items-center px-4 header-container max-w-[1440px] w-full">
+          <Link to="/">
+            <h1 className="font-bm text-2xl text-white font-normal">크루위키</h1>
+          </Link>
 
-        <div className="flex items-center">
-          <RandomButton className="mr-4 cursor-pointer" />
-          <WikiInputField className="w-20 md:w-[20.25rem] hidden md:flex" />
-          <SearchCircleSmall className="cursor-pointer md:hidden" />
+          <div className="flex items-center">
+            <RandomButton className="mr-4 cursor-pointer" />
+            <WikiInputField className="w-20 md:w-[20.25rem] hidden md:flex" />
+            <SearchCircleSmall className="cursor-pointer md:hidden" onClick={()=>setVisibleSmallSearchBar(true)} />
+          </div>
         </div>
-      </div>
+        <div className={twMerge("flex items-center md:hidden", isVisibleSmallSearchBar?"":"hidden")}>
+          <WikiInputField className="w-[20.25rem] flex" />
+          </div>
+        </div>
     </motion.div>
-  );
+);
 };
 
 export default WikiHeader;
