c2b2b7816be1fbde9513692806b676b759398578
김진호 <rlawlsgh1227@gmail.com>
2024-04-03 14:14:38 +0900

    feat: 헤더 새로고침 기능 구현

--- client/src/components/DocumentHeader.tsx
+++ client/src/components/DocumentHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import URLS from '@constants/urls';
 import { WikiDocument } from '@type/DocumentType';
+import { useQueryClient } from '@tanstack/react-query';
 import DocumentTitle from './DocumentTitle';
 import Button from './Button';
 
@@ -11,6 +12,12 @@ interface DocumentHeaderProps {
 
 const DocumentHeader = ({ wiki }: DocumentHeaderProps) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const refreshData = () => {
+    queryClient.removeQueries();
+    navigate('');
+  };
 
   const goPostPage = () => {
     navigate(URLS.POST);
@@ -28,7 +35,7 @@ const DocumentHeader = ({ wiki }: DocumentHeaderProps) => {
     <header className="max-[768px]:flex-col-reverse max-[768px]:gap-4 flex justify-between w-full">
       <DocumentTitle title={wiki.title} />
       <fieldset className="flex gap-2 max-[768px]:w-full max-[768px]:justify-center">
-        <Button style="tertiary" size="xs" text="새로고침" />
+        <Button style="tertiary" size="xs" text="새로고침" onClick={refreshData} />
         <Button style="tertiary" size="xs" text="편집하기" onClick={goEditPage} />
         <Button style="tertiary" size="xs" text="편집로그" onClick={goLogsPage} />
         <Button style="primary" size="xs" text="작성하기" onClick={goPostPage} />
