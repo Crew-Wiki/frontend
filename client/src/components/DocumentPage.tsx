4bf4946a9c08bfdc50f7151a12efa02beab41853
skiende74 <skiende74@gmail.com>
2024-04-03 16:43:35 +0900

    refactor: 768px를 md로 사용하게끔 변경

--- client/src/components/DocumentPage.tsx
+++ client/src/components/DocumentPage.tsx
@@ -47,14 +47,14 @@ const DocumentPage = ({ daemoon }: DocumentPageProps) => {
   return (
     <>
       <div className="md:hidden">
-        <fieldset className="flex gap-2 max-[768px]:w-full max-[768px]:justify-center">
+        <fieldset className="flex gap-2 max-md:w-full max-md:justify-center">
           <Button style="tertiary" size="xs" text="새로고침" onClick={refreshData} />
           <Button style="tertiary" size="xs" text="편집하기" onClick={goEditPage} />
           <Button style="tertiary" size="xs" text="편집로그" onClick={goLogsPage} />
           <Button style="primary" size="xs" text="작성하기" onClick={goPostPage} />
         </fieldset>
       </div>
-      <div className="flex flex-col gap-6 w-full h-fit min-h-[864px] bg-white border-primary-100 border-solid border rounded-xl p-8 max-[768px]:p-4 max-[768px]:gap-2">
+      <div className="flex flex-col gap-6 w-full h-fit min-h-[864px] bg-white border-primary-100 border-solid border rounded-xl p-8 max-md:p-4 max-md:gap-2">
         <DocumentHeader wiki={docs} />
         <DocumentContents contents={docs.contents} />
       </div>
