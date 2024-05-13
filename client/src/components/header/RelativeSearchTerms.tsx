916782242e0ab67aad5114fede6402e5b62b46f7
김진호 <rlawlsgh1227@gmail.com>
2024-05-13 12:59:33 +0900

    design: hover active border 수정

--- client/src/components/header/RelativeSearchTerms.tsx
+++ client/src/components/header/RelativeSearchTerms.tsx
@@ -9,13 +9,13 @@ const RelativeSearchTerms = ({ searchTerms, onSubmit }: RelativeSearchTermsProps
   return (
     <>
       {searchTerms.length !== 0 && (
-        <aside className="absolute rounded-xl shadow-2xl py-2 left-0 top-12 w-full bg-white">
+        <aside className="absolute rounded-xl shadow-2xl px-2 py-2 left-0 top-12 w-full bg-white">
           {searchTerms.map((search, index) => (
             <button
               onClick={(event) => {
                 onSubmit(event, search);
               }}
-              className="w-full px-4 py-2 hover:bg-primary-50 cursor-pointer text-left"
+              className="w-full px-2 py-2 hover:bg-primary-50 rounded-lg cursor-pointer text-left"
               key={index}
             >
               {search}
