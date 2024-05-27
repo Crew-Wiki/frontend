7518e2bb1c16cb3638945e253ae26035ffffcc00
김진호 <rlawlsgh1227@gmail.com>
2024-05-27 21:34:02 +0900

    fix: basename 수정

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -8,43 +8,46 @@ import LogContent from '@components/logs/LogContent';
 import WritePage from '@components/write/WritePage';
 import App from './App';
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <App />,
-    children: [
-      {
-        path: URLS.MAIN,
-        element: <Navigate to={`${URLS.WIKI}/${URLS.DAEMOON}`} />,
-      },
-      {
-        path: `${URLS.WIKI}/${URLS.DOCS}`,
-        element: (
-          <DocumentBoundary>
-            <DocumentPage />
-          </DocumentBoundary>
-        ),
-      },
-      {
-        path: `${URLS.WIKI}/${URLS.DOCS}/${URLS.LOGS}`,
-        element: <EditLogs />,
-      },
-      {
-        path: `${URLS.WIKI}/${URLS.DOCS}/${URLS.SPECIFIC_LOG}`,
-        element: (
-          <DocumentBoundary>
-            <LogContent />
-          </DocumentBoundary>
-        ),
-      },
-      { path: URLS.POST, element: <WritePage mode="add" /> },
-      {
-        path: URLS.EDIT,
-        element: <WritePage mode="edit" />,
-      },
-      { path: '/*', element: <Navigate to="/" /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '',
+      element: <App />,
+      children: [
+        {
+          path: URLS.MAIN,
+          element: <Navigate to={`${URLS.WIKI}/${URLS.DAEMOON}`} />,
+        },
+        {
+          path: `${URLS.WIKI}/${URLS.DOCS}`,
+          element: (
+            <DocumentBoundary>
+              <DocumentPage />
+            </DocumentBoundary>
+          ),
+        },
+        {
+          path: `${URLS.WIKI}/${URLS.DOCS}/${URLS.LOGS}`,
+          element: <EditLogs />,
+        },
+        {
+          path: `${URLS.WIKI}/${URLS.DOCS}/${URLS.SPECIFIC_LOG}`,
+          element: (
+            <DocumentBoundary>
+              <LogContent />
+            </DocumentBoundary>
+          ),
+        },
+        { path: URLS.POST, element: <WritePage mode="add" /> },
+        {
+          path: URLS.EDIT,
+          element: <WritePage mode="edit" />,
+        },
+        { path: '/*', element: <Navigate to="/" /> },
+      ],
+    },
+  ],
+  { basename: '/' },
+);
 
 export default router;
